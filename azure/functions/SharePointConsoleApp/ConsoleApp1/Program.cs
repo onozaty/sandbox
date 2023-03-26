13ba33120a610135ad42412d8ed419602cc2185f
onozaty <onozaty@gmail.com>
2023-03-26 21:20:23 +0900

    [Azure Functions] SharePointリストをMicrosoft Graphで取得

---
+++ azure/functions/SharePointConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Identity;
+using Microsoft.Graph;
+
+var tenantId = "<tenantId>";
+var clientId = "<clientId>";
+var clientSecret = "<clientSecret>";
+var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+var graphClient = new GraphServiceClient(clientSecretCredential);
+
+string siteId = "vmx76.sharepoint.com,2a3a60e5-ff3d-4c30-ae2b-c75dc6d3a10c,0e0bf9d7-d25d-4981-a873-c245b1e88c1a";
+string listId = "a9ef9676-65a2-4665-9372-1050932a82c0";
+
+var items = await graphClient.Sites[siteId].Lists[listId].Items.GetAsync((requestConfiguration) =>
+{
+    requestConfiguration.QueryParameters.Expand = new string[] { "fields" };
+});
+
+foreach (var item in items.Value)
+{
+    // リストの情報を表示する
+    Console.WriteLine($"item.Fields: {string.Join(", ", item.Fields.AdditionalData.Select(x => $"{x.Key}: {x.Value}"))}");
+}
+
