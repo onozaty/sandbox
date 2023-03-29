3a28a5a271b1efe9875ec7c073f7c50a7c711299
onozaty <onozaty@gmail.com>
2023-03-29 21:05:05 +0900

    [Azure Functions] VSCodeによるプロジェクト作成

---
+++ azure/functions/VSCodeApp/HttpTrigger1.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Graph;
+using System.Text;
+using Azure.Identity;
+
+namespace Company.Function
+{
+    public static class HttpTrigger1
+    {
+        [FunctionName("HttpTrigger1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var tenantId = Environment.GetEnvironmentVariable("TENANT_ID");
+            var clientId = Environment.GetEnvironmentVariable("CLIENT_ID");
+            var clientSecret = Environment.GetEnvironmentVariable("CLIENT_SECRET");
+
+            var clientSecretCredential = new ClientSecretCredential(
+                tenantId, clientId, clientSecret);
+
+            var graphClient = new GraphServiceClient(clientSecretCredential);
+
+            var result = await graphClient.Users.GetAsync();
+
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach(var user in result.Value) {
+                stringBuilder.AppendLine($"{user.Id}: {user.DisplayName}");
+            }
+
+            return new OkObjectResult(stringBuilder.ToString());
+        }
+    }
+}
