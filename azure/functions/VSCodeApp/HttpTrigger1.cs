f1d3c2dd8fdc382934f118bde5a4a0da4e062daf
onozaty <onozaty@gmail.com>
2023-04-06 23:13:39 +0900

    [Azure Functions] 軽微な修正

--- azure/functions/VSCodeApp/HttpTrigger1.cs
+++ azure/functions/VSCodeApp/HttpTrigger1.cs
@@ -1,12 +1,10 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using Microsoft.Graph;
 using System.Text;
 using Azure.Identity;
@@ -35,7 +33,7 @@ namespace Company.Function
 
             StringBuilder stringBuilder = new StringBuilder();
             foreach(var user in result.Value) {
-                stringBuilder.AppendLine($"{user.Id}: {user.DisplayName}");
+                stringBuilder.AppendLine($"{user.Mail}: {user.DisplayName}");
             }
 
             return new OkObjectResult(stringBuilder.ToString());
